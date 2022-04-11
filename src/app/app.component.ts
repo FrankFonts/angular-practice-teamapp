cc5700fafb8ba888dabd03d14347bdacdf26a5f9
FrankFonts <9556806+FrankFonts@users.noreply.github.com>
2022-04-11 23:01:56 +0200

    Getting the gist. Very basic, obvs. Having fun.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | "" = "" ;
+  teams: string[][] = []; // gonna be an array of arrays of strings -> TS is crazy
+  
+  onInput(member: string){
+    this.newMemberName = member;
+  }
+
+  onNumberOfTeamsInput(value: string){
+    this.numberOfTeams = Number(value);
+  }
+
+  addMember(){
+    if(!this.newMemberName){
+      this.errorMessage = "Name can't be empty";
+      return;
+    }
+    this.errorMessage='';
+    this.members.push(this.newMemberName);
+    this.newMemberName='';
+  }
+
+  generateTeams() {
+    if (!this.numberOfTeams || this.numberOfTeams <= 0){
+      this.errorMessage = "Invalid number of teams";
+      return;
+    }
+
+    if ( this.members.length < this.numberOfTeams) {
+      this.errorMessage = "Not enough members";
+      return;
+    }
+
+    this.teams = [];
+    this.errorMessage = "";
+    const allMembers = [...this.members]; // create a real copy of array
+
+    while(allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0]; // mutates array and returns a new one
+
+        if(!member) break; // just break the loop if...
+
+        if(this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+    }
+
+       
+    }
+    
+    console.log(this.teams);
+    this.members = [];
+    this.numberOfTeams = '';
+    
+  }
 }
